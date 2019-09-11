26f4360a2b1a84c5efd1a7b3a00d7efdbff3749e
gift54 <gift7333@gmail.com>
2019-09-11 07:33:57 -0700

    MY JS COMMIT

---
+++ App.js
@@ -0,0 +1,7 @@
+function N() {
+    p = document.getElementById("hello");
+    p.innerHTML="gift";
+    var div = document.getElementById("eleojo");
+    div.innerHTML="welcome to my world";
+    
+}
